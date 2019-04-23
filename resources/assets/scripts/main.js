0994005528743bc488bcf1db588b14e6ec275f24
Rydzkowski Grzegorz <grzegorz.rydzkowski@gdansk.gda.pl>
2019-04-23 12:33:45 +0200

    slider

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -4,11 +4,16 @@ import 'jquery';
 // Import everything from autoload
 import './autoload/**/*'
 
+// Import Slick
+import 'slick-carousel/slick/slick.min';
+
 // import local dependencies
 import Router from './util/Router';
 import common from './routes/common';
 import home from './routes/home';
 import aboutUs from './routes/about';
+
+
 
 /** Populate Router instance with DOM routes */
 const routes = new Router({
--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -1,6 +1,16 @@
 export default {
   init() {
     // JavaScript to be fired on the home page
+    jQuery('.slider').slick({
+      draggable: true,
+      arrows: true,
+      dots: false,
+      fade: true,
+      speed: 900,
+      infinite: true,
+      prevArrow: jQuery('.slider__arrow--prev'),
+      nextArrow: jQuery('.slider__arrow--next'),
+    });
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
