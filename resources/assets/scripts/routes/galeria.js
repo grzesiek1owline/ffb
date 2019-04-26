cf5c28d116bfc9be692eda5405bfaf34e804fbd6
Owline <biuro.owline@gmail.com>
2019-04-26 23:48:52 +0200

    v8

--- resources/assets/scripts/routes/galeria.js
+++ resources/assets/scripts/routes/galeria.js
@@ -1,7 +1,6 @@
 export default {
     init() {
       // JavaScript to be fired on the gallery
-      alert('siema');
     },
     finalize() {
       jQuery('.gallery__element a').simpleLightbox();
